6c670abaa633f5fb60046c3f60a2d48a60e52160
lqkiet <lequockiet1512@gmail.com>
2022-04-06 17:05:51 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes = [
         name: "ContactBook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: () => import("@/views/NotFound.vue"),
+        },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
